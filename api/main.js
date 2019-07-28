508e3192822571d7f5886c902eab9351ea4ca247
Vandy Cheung <vandyw95@gmail.com>
2019-07-28 15:50:29 +0700

    - Added "esm" to enable ES6 syntax for nodejs - Run "esm" as index and moved server into main.js

---
+++ api/main.js
@@ -0,0 +1,22 @@
+import { ApolloServer } from 'apollo-server-micro';
+
+import typeDefs from './type-defs';
+import resolvers from './resolvers';
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true
+});
+
+// Some have issue with request, keep this in advance
+// https://spectrum.chat/next-js/general/apollo-server-micro-as-an-api-route~6133e1f2-5f93-4dc5-9ff5-e7d1f7b86505
+// https://gist.github.com/mfellner/2119db3584023092d70118a8dabd146e#file-graphql-ts-L19-L23
+// export const config = {
+//   api: {
+//     bodyParser: false
+//   }
+// };
+
+export default apolloServer.createHandler({ path: '/api/graphql' });
